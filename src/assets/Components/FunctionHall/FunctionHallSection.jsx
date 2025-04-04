68a9b679579e19506ba31f6cc33a2a0b46b8422c
ajpogii <151542585+AjPogii@users.noreply.github.com>
2025-04-04 13:18:08 +0800

    commit changes

--- src/assets/Components/FunctionHall/FunctionHallSection.jsx
+++ src/assets/Components/FunctionHall/FunctionHallSection.jsx
@@ -1,57 +1,42 @@
 import React from 'react'
 
-const Facilities = () => {
+const FunctionHall = () => {
 
-    const facilitiesData = [
-        {
-            id: 1,
-            name: 'Single Room',
-            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.',
-            icon: '',
-        },
-
-        {
-            id: 2,
-            name: 'Double Room',
-            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.',
-            icon: '',
-        },
-        {
-            id: 3,
-            name: 'Function Hall',
-            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.',
-            icon: '',
-        },
-    ];
 
     return (
-        <div className="facilities-container section">
-            <div className="facilities-title">
-                <h1 className="section_title">Facilities</h1>
-                <p className="description">
-                    Discover a world of comfort and convenience with our exceptional facilities and services, designed to enhance your stay.
-                </p>
-            </div>
 
-            <div className="facilities-cards">
-                {facilitiesData.map((facility) => (
+        <div className="function-hall section">
 
-                    <div className="facility-card" key={facility.id}>
-                        <div className="facility-icon">{facility.icon}</div>
-
-                        <div className="facility-content">
-                            <h3 className="facility-name">{facility.name}</h3>
-                            <p className="facility-description">{facility.description}</p>
+            <div className="function-hall-container">
+                <div className="function-hall-images">
+                    <div className="image-left">
+                        <img src="" alt="" />
+                    </div>
+                    <div className="image-right">
+                        <div className="image-top-right">
+                            <img src="" alt="" />
+                        </div>
+                        <div className="image-bottom-right">
+                            <img src="" alt="" />
                         </div>
                     </div>
-                ))}
+                </div>
+
+                <div className="function-hall-content">
+
+                    <h1 className="section-title">Function Hall</h1>
+                    <p className="description">
+                        Discover a world of comfort and convenience with our exceptional facilities and services, designed to enhance your stay.
+                    </p>
+
+                    <button className="view-all-button"> View all</button>
+
+                </div>
             </div>
-            <div className="view-all-container">
-                <button className="view-all-button"> View all</button>
-            </div>
+
         </div>
 
     )
 }
 
-export default Facilities
+export default FunctionHall
