a542d8ba38fad9e2ce68277148f06b1fba141c3c
ajpogii <151542585+AjPogii@users.noreply.github.com>
2025-04-04 13:17:19 +0800

    footer and function hall commit

--- src/assets/Components/Footer/Footer.jsx
+++ src/assets/Components/Footer/Footer.jsx
@@ -21,15 +21,15 @@ const Footer = () => {
             <div className="footer-links">
                 <h3>Contact and Reservation</h3>
                 <p><i className="ri-phone-line">  0935 281 0508</i></p>
-                <p><i class="ri-mail-line"> sales@hotelinigo.ph</i></p>
-                <p>< i class="ri-links-line">  http://www.hotelinigo.ph/</i></p>
+                <p><i className="ri-mail-line"> sales@hotelinigo.ph</i></p>
+                <p>< i className="ri-links-line">  http://www.hotelinigo.ph/</i></p>
 
             </div>
 
             <div className="footer-links">
                 <h3>Hotel Address</h3>
-                <p><i class="ri-time-line"> Always Open </i></p>
-                <p> <i class="ri-home-line">  Brgy 19 Rizal Street.,  Cabangan, Legazpi City, Legazpi, Philippines, 4500</i></p>
+                <p><i className="ri-time-line"> Always Open </i></p>
+                <p> <i className="ri-home-line">  Brgy 19 Rizal Street.,  Cabangan, Legazpi City, Legazpi, Philippines, 4500</i></p>
             </div>
         </footer>
     )
